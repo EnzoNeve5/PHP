bcb9833d30e754b2e794b25c3c1f1beb8ee0c20f
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2023-01-23 11:48:31 -0300

    Add files via upload

---
+++ aula11/exercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $c = 0;
+        while ($c <= 10000) {
+            echo $c . "</br>";
+            $c += 50;
+        }
+    ?>
+</div>
+</body>
+</html>
+ 
