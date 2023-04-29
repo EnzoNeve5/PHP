b6c25829366cc32e9c3266cda6648ddae135a1fc
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2023-04-29 17:07:05 -0300

    Add files via upload

---
+++ aula16/11-implode.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $vetor[0] = "Curso";
+        $vetor[1] = "em";
+        $vetor[2] = "video";
+        $texto = implode(" ",$vetor);
+        print($texto);
+    ?>
+</div>
+</body>
+</html>
+ 
